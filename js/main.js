3d249a023c3f8403207f0c9d66321940404a9f31
Bruno Batista <brunobatysta.fla@gmail.com>
2021-09-22 16:29:27 -0300

    Método GET no Login
    Implementação do método GET na tela de Login.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+function fazerLogin(){
+    event.preventDefault()
+    let user = document.getElementById("Cusuario").value;
+    let password = document.getElementById("Csenha").value;
+    if(user === ""){
+        document.getElementById("respostaUsuario").focus
+        alert("Campo de Usuario não pode estar vazio")
+        return false;
+    }
+    if(password === ""){
+        document.getElementById("Csenha").focus
+        alert("Campo de Senha não pode estar vazio")
+        return false;
+    }
+    console.log(user)
+    console.log(password)
+    fetch(`https://biolab-back.herokuapp.com/researchers?email=${user}&password=${password}`)
+    .then(response =>{
+        if(response.status === 400 ||  response.status === 404){
+            console.log("usuario não encontrado")
+            alert("Usuario não encontrado")
+        }
+        else if(response.status === 200){
+            alert("usuario Encontrado")
+
+        }
+    })
+}
+
+
