84b46a3359789b657936247d61d953fef69ea8df
scaduda <samira.caduda@souunit.com.br>
2021-10-13 21:08:35 -0300

    fix: logo

---
+++ js/cadastrarProtocolo.js
@@ -0,0 +1,8 @@
+function cadastrarProtocolo() {
+    event.preventDefault()
+    let mock = true;
+    if( mock == true){
+        alert("Protocolo cadastrado com sucesso")
+        window.location.replace("retornoProtocolo.html")
+    }
+}
