7a3cb209426a6a7762caf9286983c86edccb4942
Bruno Batista Santos <brunobatysta.fla@gmail.com>
2021-11-29 16:11:12 -0300

    Exibição dos dados do usuario

--- js/meusDados.js
+++ js/meusDados.js
@@ -1,4 +1,11 @@
-// fetch(`https://projeto-biolab.herokuapp.com/`)
-
-let usuario = localStorage.getItem('usuarioLogado')
-console.log(usuario)
+const usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'));
+fetch(`https://projeto-biolab.herokuapp.com/researchers/${usuarioLogado.matricula}`)
+.then((response) => response.json())
+.then((data) => {
+    console.log(data)
+    document.getElementById("nome").value = data.name;
+    document.getElementById("email").value = data.email;
+    document.getElementById("senha").value = data.password;
+    document.getElementById("curriculo").value = data.curriculumLink;
+    document.getElementById("areaInteresse").value = data.areaOfInterest;
+}) ;
